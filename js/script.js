4bbe995b2bffd4145e3486485db4b2b50e6fdddf
carlos-celfo <cagelvis.pt@gmail.com>
2022-01-05 20:43:31 +0000

    First exercise, declaring variables

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+const buttonGuess = document.querySelector(".guess");
+const letterInput = document.querySelector(".letter");
+const wordInProgress = document.querySelector(".word-in-progress");
+const remainingGuess = document.querySelector(".remaining");
+const span = document.querySelector("span");
+const message = document.querySelector(".message");
+const hiddenButton = document.querySelector(".play-again hide");
+const word = magnolia;
