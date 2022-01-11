2b79812d93a6482843665fe59f388dbdeb1fd93e
carlos-celfo <cagelvis.pt@gmail.com>
2022-01-11 22:12:11 +0000

    Event listener for the button added

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ const buttonGuess = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remainingGuess = document.querySelector(".remaining");
-const span = document.querySelector("span");
+const span = document.querySelector(".remaining span");
 const message = document.querySelector(".message");
-const hiddenButton = document.querySelector(".play-again hide");
+const hiddenButton = document.querySelector(".play-again");
 const word = "magnolia";
 
 // Placeholders for the words with ● //
@@ -19,3 +19,12 @@ const lettersUnkown = function () {
 };
 
 lettersUnkown(word);
+
+// Event listener for the button //
+
+buttonGuess.addEventListener("click", function (e) {
+  e.preventDefault();
+  const guess = letterInput.value;
+  console.log(guess);
+  letterInput.value = "";
+});
